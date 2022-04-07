f758ed40acb917d13fe49a24318a5cb9b83055ed
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-04-07 11:00:10 +0200

    lab6

--- Lab6OOP/Program.cs
+++ Lab6OOP/Program.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using System.Collections;
 using System;
 
-namespace Lab6OOP
+namespace Zoo
 {
     internal class Program
     {
@@ -9,197 +9,294 @@ namespace Lab6OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Zoo animal = new Zoo(8, 5, 1);
-            Animals Kangaroo = new Animals(9, 5, 1, 2);
-            Animals Lion = new Animals(10, 5, 1, 2);
-            Animals Cat = new Animals(11, 5, 1, 2);
-            Animals Bear = new Animals(12, 5, 1, 2);
-            Birds Parrot = new Birds();
-            Birds Woodpecker = new Birds();
-            Birds Eagle = new Birds();
-            Reptiles Crocodile = new Reptiles();
-            Reptiles Snake = new Reptiles();
-            Reptiles turtles = new Reptiles();
-            //p.DoSomeThingVirtual();
-            //c.DoSomeThingVirtual();
-            //sc.DoSomeThingVirtual();
-            
-            //// The magic begins here ...  (the logic)
-            //Parent parentTypeChildInstance = new Child(8, 7, 4, 5);
-            //parentTypeChildInstance.DoSomeThingVirtual();
-            DoSomething(animal, "animal","debend on type",0);
-            DoSomething(Kangaroo, "Kangaroo", "plants and vegetables",7);
-            DoSomething(Parrot, "Parrot", "fresh vegetables, fruit and pellets or seeds",12);
-            DoSomething(Crocodile, "Crocodile", "insects, fish, small frogs, lizards, crustaceans and small mammals",17);
+            ArrayList Animals = new ArrayList();
 
-        }
+            // Instatiate animals from concrete classes
+            PolarBear Branson = new PolarBear("Branson the Polar Bear", "Male");
+            BarnOwl Otah = new BarnOwl("Otah the Barn Owl", "Female");
+            Tiger Tiaga = new Tiger("Tiaga the Tiger", "Female");
+            BaldEagle Glory = new BaldEagle("Glory the Bald Eagle", "Female");
+            BoaConstrictor Balthazar = new BoaConstrictor("Balthazar the Boa Constrictor", "Male");
 
-        static void DoSomething(Zoo P,string name,string eat, int sleep )
-        {
-            P.DoSomeThingVirtual();
-            P.DoSomeThingNormal(name);
-            P.eat(eat);
-            P.sleep(sleep);
-            Console.WriteLine();
+            // Add animals to list
+            Animals.Add(Branson);
+            Animals.Add(Otah);
+            Animals.Add(Tiaga);
+            Animals.Add(Glory);
+            Animals.Add(Balthazar);
+
+            foreach (Animal animal in Animals) // Have each animal identify itself and then speak
+            {
+                Console.WriteLine($"{animal.Identity}:");
+                animal.Speak();
+            }
+            Console.ReadKey();
+
         }
     }
 
 
-    class Zoo                                // Parent - Super - Base class                       
+    public abstract class Animal                                // Parent - Super - Base class                       
     {
-        protected int ProtectedVarInParent;     // can only be accessed from the same class or children classes  
-        public int PublicVarInParent;           // can be accessed from anywhere
-        private int PrivateVarInParent;         // can be accessed only from inside the class
+        public abstract string Identity { get; set; }
+        public abstract string Sex { get; set; }
+        public virtual string Habitat { get; set; } = "None";
+        public virtual string Diet { get; set; } = "Any";
+        public virtual bool Nocturnal { get; set; } = false;
+        public virtual bool ColdBlooded { get; set; } = false;
 
-        public Zoo(int PrivateVarInParent)
-        {
-            this.PrivateVarInParent = PrivateVarInParent;
-        }
+        abstract public void Speak();
 
-        public Zoo(int ProtectedVarInParent, int PublicVarInParent, int PrivateVarInParent)
+        virtual public void Rest()
         {
-            this.ProtectedVarInParent = ProtectedVarInParent;
-            this.PublicVarInParent = PublicVarInParent;
-            this.PrivateVarInParent = PrivateVarInParent;
+            Console.WriteLine($"{Identity} falls asleep.");
         }
-
-        public virtual void DoSomeThingVirtual()
-        {
-            Console.WriteLine("This was done by the Zoo virtual function");
-        }
-
-        public void DoSomeThingNormal(string anm)
-        {
-            Console.WriteLine($"This was {anm} in my  Zoo ");
-        }
-        public void eat(string anm)
-        {
-            Console.WriteLine($"This was eat of {anm} ");
-        }
-        public void sleep(int anm)
-        {
-            Console.WriteLine($"This animals was sleep : {anm} Hr ");
-        }
-
 
     }
-
-    class Animals : Zoo                        // Child inherits Parent - Child - Sub - Derived          
+    public abstract class Bear : Mammal
     {
-        private int PrivateVarInChild;
 
-        // As we don't have a parameterless constructor in parent, then we *Have To* call the construcor with parameters 
-        public Animals(int ProtectedVarInParent, int PublicVarInParent, int PrivateVarInParent, int PrivateVarInChild) : base(ProtectedVarInParent, PublicVarInParent, PrivateVarInParent)
+        public virtual void Hibernate()
         {
-            this.PrivateVarInChild = PrivateVarInChild;
+            Console.WriteLine($"{Identity} settles down to hibernate for the winter.");
         }
-
-        public Animals(int PrivateVarInParent, int PrivateVarInChild) : base(PrivateVarInParent)
-        {
-            this.PrivateVarInChild = PrivateVarInChild;
-        }
-
-        public override void DoSomeThingVirtual()
-        {
-            Console.WriteLine("This was done by the child class");
-        }
-
-        public new void DoSomeThingNormal()
-        {
-            Console.WriteLine("This was done by the child function");
-        }
-
-        public void ThisIsOnlyInFirstChild()
-        {
-            Console.WriteLine("This is the child's own function");
-        }
-        
     }
-
-    class Birds : Zoo
+    public abstract class Bird : Animal
     {
-        public Birds() : base(5)
-        {
-
-        }
-
-        public override void DoSomeThingVirtual()
-        {
-            Console.WriteLine("This was done by the Birds class");
-        }
-       
+        virtual public bool CanFly { get; set; } = true;
+        abstract public string FeatherPattern { get; set; }
 
     }
-    class Reptiles : Zoo
+    public class BoaConstrictor : Snake
     {
-        public Reptiles() : base(5)
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore, small and medium-sized mammals";
+        public override string Habitat { get; set; } = "Jungle";
+        public override string ScalePattern { get; set; } = "Brown, gray, or cream base color with brown or reddish-brown saddle patterns";
+        public BoaConstrictor(string identity, string sex)
         {
-
+            Identity = identity;
+            Sex = sex;
         }
 
-        public override void DoSomeThingVirtual()
+        public override void Speak()
         {
-            Console.WriteLine("This was done by the Reptiles class");
+            Console.WriteLine("HSSSSSSSSSSSSsssss");
         }
+        public override void Slither()
+        {
+            Console.WriteLine($"{Identity} slithers across the ground.");
+        }
+    }
+
+    public abstract class Cat : Mammal
+    {
+        public string Prey { get => Diet; }
+        virtual public void Pounce(string targetId)
+        {
+            Console.WriteLine($"{Identity} pounces at {targetId}");
+        }
+        public bool SmellForPrey()
+        {
+            return false;
+        }
+        public bool CatchPrey()
+        {
+            return false;
+        }
+    }
+
+    public class EmperorPenguin : Penguin
+    {
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore, fish";
+        public override string Habitat { get; set; } = "Mountains near bodies of water";
+        public override string FeatherPattern { get; set; } = "White head with dark brown body";
+        public EmperorPenguin(string identity, string sex)
+        {
+            Identity = identity;
+            Sex = sex;
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("MraaAAW");
+        }
+    }
+
+
+    public abstract class Mammal : Animal
+    {
+        public abstract string FurColor { get; set; }
+        virtual public void Bite(string target)
+        {
+            Console.WriteLine($"{Identity} bites {target}");
+        }
+    }
+
+    public abstract class Owl : Bird
+    {
+        override public bool Nocturnal { get; set; } = true;
+        public string Prey { get => Diet; }
+        public bool SmellForPrey()
+        {
+            return false;
+        }
+        public bool CatchPrey()
+        {
+            return false;
+        }
+        public int TopFlySpeed { get; set; }
+        public int TakeOff()
+        {
+            Console.WriteLine($"{Identity} flaps their wings and takes flight.");
+            return TopFlySpeed;
+        }
+        public bool Land()
+        {
+            Console.WriteLine($"{Identity} lands.");
+            return true;
+        }
+
 
     }
+
+    public abstract class Penguin : Bird
+    {
+
+    }
+    public class PolarBear : Bear
+    {
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore, medium-sized mammals";
+        public override string Habitat { get; set; } = "Arctic";
+        public override string FurColor { get; set; } = "White";
+        public string Prey { get => Diet; }
+        public PolarBear(string identity, string sex)
+        {
+            Identity = identity;
+            Sex = sex;
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("RAhr RAhr");
+        }
+
+        public bool SmellForPrey()
+        {
+            return false;
+        }
+        public bool CatchPrey()
+        {
+            return false;
+        }
+
+
+    }
+
+    public abstract class Reptile : Animal
+    {
+        abstract public string ScalePattern { get; set; }
+        public override bool ColdBlooded { get; set; } = true;
+    }
+
+    public abstract class Snake : Reptile
+    {
+        public string Prey { get => Diet; }
+        public abstract void Slither();
+        public bool SmellForPrey()
+        {
+            return false;
+        }
+        public bool CatchPrey()
+        {
+            return false;
+        }
+    }
+
+    public class Tiger : Cat
+    {
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore";
+        public override string Habitat { get; set; } = "Jungle";
+        public override string FurColor { get; set; } = "Orange and black stripes";
+        public Tiger(string identity, string sex)
+        {
+            Identity = identity;
+            Sex = sex;
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Grrr...RAWR!");
+        }
+    }
+
+    public abstract class Eagle : Bird
+    {
+        public string Prey { get => Diet; }
+        public bool SmellForPrey()
+        {
+            return false;
+        }
+        public bool CatchPrey()
+        {
+            return false;
+        }
+
+        public int TopFlySpeed { get; set; }
+        public int TakeOff()
+        {
+            Console.WriteLine($"{Identity} flaps their wings and takes flight.");
+            return TopFlySpeed;
+        }
+        public bool Land()
+        {
+            Console.WriteLine($"{Identity} lands.");
+            return true;
+        }
+    }
+    public class BarnOwl : Owl
+    {
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore, small mammals";
+        public override string Habitat { get; set; } = "Forest";
+        public override string FeatherPattern { get; set; } = "White, heart-shaped face with mottled brown ";
+        public BarnOwl(string identity, string sex)
+        {
+            Identity = identity;
+            Sex = sex;
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("EEEEEEEK");
+        }
+    }
+
+    public class BaldEagle : Eagle
+    {
+        public override string Identity { get; set; }
+        public override string Sex { get; set; }
+        public override string Diet { get; set; } = "Carnivore, fish";
+        public override string Habitat { get; set; } = "Mountains near bodies of water";
+        public override string FeatherPattern { get; set; } = "White head with dark brown body";
+        public BaldEagle(string identity, string sex)
+        {
+            Identity = identity;
+            Sex = sex;
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("EEE ee E eeee");
+        }
+    }
+    
 
 }
-
-
-
-
-//using System;
-
-//namespace Lab6OOP
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//            Zoo animals = new Zoo("crocodais", "any");
-
-//        }
-//    }
-
-//    class Zoo
-//    {
-//        public string MyProperty { get; set; }
-//        public string MyValue;
-//        public string MyValue2;
-//        string[] content;
-
-//        public void Eat()
-//        {
-//            string allData = MyValue + "value1" + MyValue2 + "value2";
-//            Console.WriteLine(allData);
-
-//        }
-
-//        public Zoo(string myvalue, string myvalue2, string[] content)
-//        {
-
-//           this.MyValue = myvalue;
-//            this.MyValue2 = myvalue2;
-//            this.content = content;
-//        }
-
-
-
-//    }
-//    class animals : Zoo
-//    {
-//        string foot;
-//        string foot2;
-//        public animals() : base()
-
-//        {
-
-//           this.foot = "test";
-//            this.foot2 = "test";
-
-
-//        }
-//    }
-//}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Lab6OOP
+# Lab6OOP - OOP Principles
+### Description
+This is a console application made to simulate a zoo in order to showcase the principles of OOP.
+#### Getting Started
+Clone this repository to your local machine.
+
+#### To run the program from Visual Studio:
+Select File -> Open -> Project/Solution
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the Lab06-OOPPrinciples directory.
+
+Then select and open Lab06-OOPPrinciples.sln
+
+#### Visuals
+UML
+
+
+### OOP Principles
+##### Inheritance
+- Expresses a Parent/Child relationship between two classes wherein one class can inherit the methods and properties of another. In this code, inheritance is shown by the Concrete animal classes applying properties from their parent classes as well as using methods defined in the parent classes.
+##### Abstraction
+- The act of taking a real world example and breaking it down into fundamental parts or abstracting it to a more basic form. In the code, this is shown by the concrete examples of animals like giraffes being abstracted to herbivores, which is further abstracted to mammals, and even further to animals.
+##### Encapsulation
+- Encapsulation is the act of "wrapping" data up so that it can only be accessed in specific, predefined ways and it's actual data holds integrity. In the code, we only use public because we needed to write XUnit tests for all the methods, but private methods should be applied only on the concrete level, protected methods would exist in the parents of the concrete animals and internal methods could exist anywhere in the application.
+##### Polymorphism
+- Polymorphism is the ability for certain inherited traits to be changed or overridden by deriving classes. In our code, polymorphism is shown in the method sleep which is overridden and changed several times in different levels of inheritance.
